f9eb90e33fec11b5b4b5a7fccb5154297a3d6479
Anis <imarahmandev@gmail.com>
2022-10-03 13:56:18 +0700

    applying middleware

--- app/store.js
+++ app/store.js
@@ -1,13 +1,16 @@
-const { configureStore } = require('@reduxjs/toolkit')
-
+const { configureStore, getDefaultMiddleware } = require('@reduxjs/toolkit')
+const reduxLogger = require('redux-logger')
 const cakeReducer = require('../features/cake/cakeSlice')
 const iceCreamReducer = require('../features/icecream/iceCreamSlice')
+
+const logger = reduxLogger.createLogger()
 
 const store = configureStore({
     reducer: {
         cake: cakeReducer,
         icecream: iceCreamReducer
-    }
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
 })
 
 
--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 const store = require('./app/store')
 const { cakeActions } = require('./features/cake/cakeSlice')
 const { iceCreamActions } = require('./features/icecream/iceCreamSlice')
-console.log('initial ', store.getState());
-
-const unsubscribe = store.subscribe(() => {
-    console.log("update store ", store.getState())
-});
-
 
 store.dispatch(cakeActions.ordered())
 store.dispatch(cakeActions.ordered())
@@ -21,7 +15,3 @@ store.dispatch(iceCreamActions.ordered())
 store.dispatch(iceCreamActions.ordered())
 store.dispatch(iceCreamActions.ordered())
 store.dispatch(iceCreamActions.ordered())
-
-
-
-unsubscribe()
